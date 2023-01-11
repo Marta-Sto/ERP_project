9c95f50afcf4b97d761ea9dfa9d142f6e7d9d970
Marta Stojanovic <78546959+Marta-Sto@users.noreply.github.com>
2023-01-11 09:30:59 +0100

    Update pipeline.py
    Update comments in file to plot cluster results/significant clusters across subjects.

--- pipeline.py
+++ pipeline.py
@@ -10,21 +10,21 @@ Created on Thu Jan  5 01:50:56 2023
 import mne
 import numpy as np
 from data_across_sub import across_sub
-from scripttoplot import plot_cluster_results
+#from scripttoplot import plot_cluster_results
+from plot_clusters import plot_cluster_results
 
 
 ############ PIPELINE ############
 
 data_path = '/Users/stojanovic/Documents/Projects/Cambridge/Analyses/Adaptor/NewEEGFilesAdaptor'
 
-
 # Load dict of two lists, average for each subject for first and last repetition
 ERP = across_sub(data_path) 
 
 ### Get array to pass to permutation cluster test using list comprehension ###
-X = np.vstack([first.data.mean(axis=0) - last.data.mean(axis=0) for first,last in zip(ERP['0'],ERP['3'])])
+X = np.vstack([first.data.mean(axis=0) - last.data.mean(axis=0) for first,last in zip(ERP['0'],ERP['4'])]) # can change to 3
 
 diff_stat = mne.stats.permutation_cluster_1samp_test(X) 
 
-# ### Plot cluster results ###
+### Plot cluster results ###
 cluster_results = plot_cluster_results(ERP, diff_stat)
